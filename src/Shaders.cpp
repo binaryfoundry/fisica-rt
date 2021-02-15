cb69fc33e7c2c54c275c0107555cb7f4fa1053f3
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2021-02-15 21:48:35 +0000

    Shader Code Style Fixes

--- src/Shaders.cpp
+++ src/Shaders.cpp
@@ -10,8 +10,7 @@ std::string frontbuffer_vertex_shader_string =
     layout(location = 0) in vec3 position;
     layout(location = 1) in vec2 texcoord;
     out vec2 v_texcoord;
-    void main()
-    {
+    void main() {
         v_texcoord = texcoord;
         gl_Position = projection * view * vec4(position, 1.0);
     })";
@@ -33,15 +32,14 @@ std::string frontbuffer_fragment_shader_string =
          1.60475, -0.53108, -0.07367,
         -0.10208,  1.10813, -0.00605,
         -0.00327, -0.07276,  1.07602);
-    vec3 RRTAndODTFit(vec3 v)
-    {
+
+    vec3 RRTAndODTFit(vec3 v) {
         vec3 a = v * (v + 0.0245786) - 0.000090537;
         vec3 b = v * (0.983729 * v + 0.4329510) + 0.238081;
         return a / b;
     }
 
-    vec3 tone_map(vec3 color, float exposure)
-    {
+    vec3 tone_map(vec3 color, float exposure) {
         color = (color * exposure) * ACESInputMat;
         color = RRTAndODTFit(color);
         color = color * ACESOutputMat;
@@ -53,8 +51,7 @@ std::string frontbuffer_fragment_shader_string =
     vec3 to_linear_approx(vec3 v) { return pow(v, vec3(gamma)); }
     vec3 to_gamma_approx(vec3 v) { return pow(v, vec3(1.0 / gamma)); }
 
-    void main()
-    {
+    void main() {
         vec3 c = texture(tex, v_texcoord).xyz;
         out_color = vec4(to_gamma_approx(tone_map(c, 1.0f)), 1.0);
     })";
