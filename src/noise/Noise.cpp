80fe061eddd8f6bb29512b071f54e658aeb15d06
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2021-02-27 07:36:40 +0000

    Add Noise Samplers as Header Library

---
+++ src/noise/Noise.cpp
@@ -0,0 +1,3 @@
+#include "Noise.hpp"
+
+#include <noise/samplerBlueNoiseErrorDistribution_128x128_OptimizedFor_4spp.h>
--- src/noise/Sampler4SPP.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace Noise
-{
-    float sampler_128x128_OptimizedFor_2d2d2d2d_4spp(
-        int pixel_i, int pixel_j, int sampleIndex, int sampleDimension);
-}
